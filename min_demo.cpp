76195709f775e8d2a47806deaa5b2fe82eec3f64
cag2050 <cag2050@163.com>
2020-10-21 20:17:10 +0800

    add: demo

---
+++ min_demo.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    double correct_use_time = 0.0;
+    correct_use_time = std::min((double)11.0, (double)3600.0);
+    std::cout << correct_use_time << std::endl;
+}
